64cabe4991d28256694b5aadabb3e36497b98ce0
Diego <dgcostapinto@gmail.com>
2023-08-12 20:12:29 -0300

    Adicionando mensagem de erro

--- main.py
+++ main.py
@@ -24,30 +24,34 @@ class CorreiosTracker:
     def rastrear_encomenda(self, codigo_rastreamento):
         url = f"https://api.linketrack.com/track/json?user=teste&token=1abcd00b2731640e886fb41a8a9671ad1434c599dbaa0a0de9a5aa619f29a83f&codigo={codigo_rastreamento}"
 
-        response = requests.get(url)
-        data = response.json()
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+            if "erro" in data:
+                return [{"error": "Código de rastreamento inválido."}]
+            else:
+                eventos = data.get("eventos", [])
+                eventos_formatados = []
+                for i in eventos:
+                    data_evento = i.get("data", "")
+                    hora_evento = i.get("hora", "")
+                    local_evento = i.get("local", "")
+                    status_evento = i.get("status", "")
+                    sub_status_evento = i.get("subStatus", "")
 
-        if "erro" in data:
-            return [{"error": "Código de rastreamento inválido."}]
-        else:
-            eventos = data.get("eventos", [])
-            eventos_formatados = []
-            for i in eventos:
-                data_evento = i.get("data", "")
-                hora_evento = i.get("hora", "")
-                local_evento = i.get("local", "")
-                status_evento = i.get("status", "")
-                sub_status_evento = i.get("subStatus", "")
-
-                evento_formatado = {
-                    "data": data_evento,
-                    "hora": hora_evento,
-                    "local": local_evento,
-                    "status": status_evento,
-                    "subStatus": sub_status_evento
-                }
-                eventos_formatados.append(evento_formatado)
-            return eventos_formatados
+                    evento_formatado = {
+                        "data": data_evento,
+                        "hora": hora_evento,
+                        "local": local_evento,
+                        "status": status_evento,
+                        "subStatus": sub_status_evento
+                    }
+                    eventos_formatados.append(evento_formatado)
+                return eventos_formatados
+        except requests.exceptions.HTTPError as e:
+            messagebox.showerror("Erro", "Erro ao tentar se conectar com o servidor.")
+            return [{"error": f"Erro ao tentar se conectar com o servidor. {str(e)} "}]
 
     def rastrear_click(self):
         codigo = self.codigo_entry.get()
@@ -60,8 +64,10 @@ class CorreiosTracker:
             self.resultado_text.insert(tk.END, pp_result)
             self.resultado_text.config(state=tk.DISABLED)
         else:
-                    messagebox.showerror("Erro", "Insira um código de rastreamento válido.")
+            messagebox.showerror("Erro", "Insira um código de rastreamento válido.")
         self.carregamento_label.pack_forget()
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = CorreiosTracker(root)
